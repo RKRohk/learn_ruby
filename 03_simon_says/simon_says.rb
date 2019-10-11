46b5c35b22072858ff261ed78fe98bebfe478aec
RKRohk <rkakar2000@gmail.com>
2019-10-12 01:20:45 +0530

    Finished Simon Says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(word)
+    word
+end
+def shout(words)
+    words.upcase
+end
+def repeat(words = "meh ",repeat = 2)
+    return_string = words
+    repeat = repeat -1
+    repeat.times do
+        return_string = return_string + " " + words 
+    end
+    return_string
+end
+def start_of_word(str,limit)
+    limit = limit -1 
+    str[0..limit]
+end
+
+def first_word(str)
+    word = str.split(' ')
+    word.first
+end
+def titleize(words)
+    collection = words.split(' ')
+    smallwords = ['an','on','over','in','the','and']
+    collection.size.times do |i|
+        if(i == 0 || !smallwords.include?(collection[i]))
+            collection[i] = collection[i].capitalize
+        end
+    end
+    words = collection.join(" ")
+    words
+end
+titleize("jaws")
