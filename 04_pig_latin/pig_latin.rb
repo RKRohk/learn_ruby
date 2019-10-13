a7e10364ea95b5423372bcd6b94b4d5891ec810a
RKRohk <rkakar2000@gmail.com>
2019-10-14 00:43:41 +0530

    Finished Pig Latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+def translate(arg="eat pie")
+    vowels = /[aeiou]/
+    phoneme = /qu/
+    is_phoneme = false
+    word_array = arg.split()
+    result = []
+    word_array.each { |words|
+        if words.match(phoneme)
+            is_phoneme = true
+        end
+        words = words.split("")
+        until (words[0].match(vowels))
+            words.push(words.shift())
+        end
+        if is_phoneme == true
+            words.push(words.shift())
+        end
+        words.push('ay')
+        new_string = words.join("")
+        result.push(new_string)
+        is_phoneme= false
+    }
+    result = result.join(" ")
+    result
+end
