bb5af276b3eb0609c173fa4cc9d681206cc2860b
RKRohk <rkakar2000@gmail.com>
2019-10-14 22:37:17 +0530

    finished timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
 class Timer
+  attr_reader :seconds, :minutes, :hours, :time_string
+  def initialize()
+    @hours = 0
+    @minutes = 0 
+    @seconds = 0
+    @time_string = "00:00:00"
+  end
+  def seconds=(seconds)
+    @seconds = seconds
+    format_string()
+  end
+
+  def format_string()
+    @hours = @seconds / 3600
+    @minutes = (seconds - hours * 3600 )/60
+    @seconds = @seconds % 60
+    @time_string = "%02d:%02d:%02d" %[hours,minutes,seconds]
+  end
   #write your code here
 end
